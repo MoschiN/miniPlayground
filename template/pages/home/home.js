aaa831cdc04198bc8a7007d9eda59d3b12c01e93
NY <icecreambo_ob@163.com>
2020-05-05 03:48:30 +0800

    问题列表完成

--- template/pages/home/home.js
+++ template/pages/home/home.js
@@ -7,6 +7,41 @@ options:{
 data:{
 StatusBar: app.globalData.StatusBar,
 CustomBar: app.globalData.CustomBar,
+checkBox:[{
+  value:0,
+  detail: '男',
+  checked: false
+},
+  {
+    value: 1,
+    detail: '女',
+    checked: false
+  }],
+  checkBox1: [{
+    value: 0,
+    detail: '体育',
+    checked: false
+  },
+  {
+    value: 1,
+    detail: '地理',
+    checked: false
+  },
+    {
+      value: 2,
+      detail: '化学',
+      checked: false
+    },
+    {
+      value: 3,
+      detail: '生物',
+      checked: false
+    },
+    {
+      value: 4,
+      detail: '语文',
+      checked: false
+    }],
 },
 methods:{
   showModal(e) {
@@ -23,6 +58,19 @@ methods:{
     this.setData({
       languageChanger: e.detail.value
     });
-  },
+  }, 
+  ChooseCheckbox(e) {
+    let items = this.data.checkBox;
+    let values = e.currentTarget.dataset.target.value;
+    for (let i = 0, lenI = items.length; i < lenI; ++i) {
+      if (items[i].value == values) {
+        items[i].checked = !items[i].checked;
+        break
+      }
+    }
+     this.setData({
+       checkBox: items
+    })
+  }
 }
 });
