9f4510d4180c56384194e22810db05f1c2f344a9
NY <icecreambo_ob@163.com>
2020-05-05 13:49:43 +0800

    result页面

--- template/pages/result/result.js
+++ template/pages/result/result.js
@@ -1,66 +1,20 @@
-// pages/result/result.js
-Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+Component({
+options: {
+  addGlobalClass:true,
+},
+data:{
+totalPersons: 10,   //总人数
+respondents:8,      //实际答题人数
+averageDuration: 0.5,  //平均答题时间
+loading:true,
+},
+//  attached() {
+// //统计算法
+//    console.log("success")
+//    let that = this;
+//    wx.showLoading({
+//      title: '数据加载中',
+//      mask: true,
+//    })
+//   }
 })
