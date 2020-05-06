76855342a8073893e6f126e54730429e673cb00a
NY <icecreambo_ob@163.com>
2020-05-06 16:52:06 +0800

    地图导航

--- template/pages/Main/home_main.js
+++ template/pages/Main/home_main.js
@@ -1,4 +1,5 @@
 // pages/Main/home_main.js
+
 Page({
 
   /**
@@ -7,60 +8,17 @@ Page({
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  formSubmit(e) {
+      let plugin = requirePlugin('routePlan');
+  let key = 'FHLBZ-RBN3I-R2HG4-5QL4P-MOWCO-6ABDG';  //使用在腾讯位置服务申请的key
+  let referer = 'ColorUI-template-Question';   //调用插件的app的名称
+  let endPoint = JSON.stringify({  //终点
+    'name': '吉野家(北京西站北口店)',
+    'latitude': 39.89631551,
+    'longitude': 116.323459711
+  });
+  wx.navigateTo({
+    url: 'plugin://routePlan/index?key=' + key + '&referer=' + referer + '&endPoint=' + endPoint
+  });
   }
 })
