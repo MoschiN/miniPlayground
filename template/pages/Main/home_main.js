46dce20423237554e894eec683b36bd27f69f79f
NY <icecreambo_ob@163.com>
2020-05-07 16:55:55 +0800

    调通了导航

--- template/pages/Main/home_main.js
+++ template/pages/Main/home_main.js
@@ -10,12 +10,12 @@ Page({
   },
   formSubmit(e) {
       let plugin = requirePlugin('routePlan');
-  let key = 'FHLBZ-RBN3I-R2HG4-5QL4P-MOWCO-6ABDG';  //使用在腾讯位置服务申请的key
+    let key = 'RGJBZ-JA6WS-HIFOW-62DQR-QBNFT-RHBWK';  //使用在腾讯位置服务申请的key
   let referer = 'ColorUI-template-Question';   //调用插件的app的名称
   let endPoint = JSON.stringify({  //终点
-    'name': '吉野家(北京西站北口店)',
-    'latitude': 39.89631551,
-    'longitude': 116.323459711
+    // 'name': '吉野家(北京西站北口店)',
+    // 'latitude': 39.89631551,
+    // 'longitude': 116.323459711
   });
   wx.navigateTo({
     url: 'plugin://routePlan/index?key=' + key + '&referer=' + referer + '&endPoint=' + endPoint
