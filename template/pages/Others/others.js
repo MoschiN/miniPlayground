5167a70cb4f758969cca8524efb554095766d7d4
NY <icecreambo_ob@163.com>
2020-05-08 16:54:42 +0800

    百度轨迹

--- template/pages/Others/others.js
+++ template/pages/Others/others.js
@@ -1,66 +1,145 @@
-// pages/Others/others.js
+//index.js
+//获取应用实例
+const app = getApp()
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    StatusBar: app.globalData.StatusBar,
+    CustomBar: app.globalData.CustomBar,
+    height: wx.getSystemInfoSync().windowHeight,
+    latitude: 0,
+    longitude: 0,
+    playIndex: 0,
+    timer: null,
+    markers: [],
+    polyline: [],
+    pointsInfo: []
+  },
+  regionchange(e) {
+    //console.log(e.type)
+  },
+  markertap(e) {
+    //console.log(e.markerId)
+  },
+  controltap(e) {
+    //console.log(e.controlId)
+  },
+  beginTrack: function (e) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    var that = this;
+    wx.request({
+      url: 'http://**/getTrack', 
+      data: {
+        beginTime: "开始时间",
+        endTime: "结束时间"
+      },
+      method: "post",
+      success: function (res) {
+        that.setData({
+          pointsInfo: res.data.pointsInfos,
+          polyline: [{
+            points: res.data.points,
+            color: "#FF0000DD",
+            width: 4,
+            dottedLine: true
+          }],
+          markers: [{
+            iconPath: '../../img/location.jpg',
+            id: 0,
+            latitude: res.data.points[0].latitude,
+            longitude: res.data.points[0].longitude,
+            width: 30,
+            height: 30,
+            title: that.data.brandNumber,
+            callout: {
+              content: that.data.brandNumber + ' \n 时间：' + res.data.pointsInfos[0].create_time + ' \n 速度：' + res.data.pointsInfos[0].speed + ' km/h',
+              color: "#000000",
+              fontSize: 13,
+              borderRadius: 2,
+              bgColor: "#fff",
+              display: "ALWAYS",
+              boxShadow: "5px 5px 10px #aaa"
+            }
+          }],
+          latitude: res.data.points[0].latitude,
+          longitude: res.data.points[0].longitude,
+        })
+      }
+    })
   },
-
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * 开始
    */
-  onReady: function () {
-
+  beginTrack: function () {
+    var that = this;
+    var i = that.data.playIndex == 0 ? 0 : that.data.playIndex;
+    that.timer = setInterval(function () {
+      i++
+      that.setData({
+        playIndex: i,
+        latitude: that.data.polyline[0].points[i].latitude,
+        longitude: that.data.polyline[0].points[i].longitude,
+        markers: [{
+          iconPath: '../../img/car/e0.png',
+          id: 0,
+          latitude: that.data.polyline[0].points[i].latitude,
+          longitude: that.data.polyline[0].points[i].longitude,
+          width: 30,
+          height: 30,
+          title: that.data.brandNumber,
+          callout: {
+            content: that.data.brandNumber + ' \n 时间：' + that.data.pointsInfo[i].create_time + ' \n 速度：' + that.data.pointsInfo[i].speed + ' km/h',
+            color: "#000000",
+            fontSize: 13,
+            borderRadius: 2,
+            bgColor: "#fff",
+            display: "ALWAYS",
+            boxShadow: "5px 5px 10px #aaa"
+          }
+        }]
+      })
+      if ((i + 1) >= that.data.polyline[0].points.length) {
+        that.endTrack();
+      }
+    }, 500)
   },
-
   /**
-   * 生命周期函数--监听页面显示
+   * 暂停
    */
-  onShow: function () {
-
+  pauseTrack: function () {
+    var that = this;
+    clearInterval(this.timer)
   },
-
   /**
-   * 生命周期函数--监听页面隐藏
+   * 结束
    */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  endTrack: function () {
+    var that = this;
+    that.setData({
+      playIndex: 0,
+      latitude: that.data.polyline[0].points[0].latitude,
+      longitude: that.data.polyline[0].points[0].longitude,
+      markers: [{
+        iconPath: '../../img/car/e0.png',
+        id: 0,
+        latitude: that.data.polyline[0].points[0].latitude,
+        longitude: that.data.polyline[0].points[0].longitude,
+        width: 30,
+        height: 30,
+        title: that.data.brandNumber,
+        callout: {
+          content: that.data.brandNumber + ' \n 时间：' + that.data.pointsInfo[0].create_time + ' \n 速度：' + that.data.pointsInfo[0].speed + ' km/h',
+          color: "#000000",
+          fontSize: 13,
+          borderRadius: 2,
+          bgColor: "#fff",
+          display: "ALWAYS",
+          boxShadow: "5px 5px 10px #aaa"
+        }
+      }]
+    })
+    clearInterval(this.timer)
   }
 })
