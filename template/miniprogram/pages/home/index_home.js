f1a6bd053afcb812893e4248c44d7e3cc8ead68c
NY <icecreambo_ob@163.com>
2020-05-20 02:00:54 +0800

    地图

--- template/miniprogram/pages/home/index_home.js
+++ template/miniprogram/pages/home/index_home.js
@@ -4,17 +4,22 @@ Component({
     addGlobalClass: true,
   },
   lifetimes: {
+  
     attached: function () {
+      this.mapCtx = wx.createMapContext('myMap')
+      this.mapCtx.moveToLocation()//将当前位置移动到地图中心
       // 在组件实例进入页面节点树时执行
       console.log('在组件实例进入页面节点树时执行')
+      var that = this
       wx.getLocation({
-        type: 'wgs84',
+        
+        type: 'gcj02', //工具中定位模拟使用IP定位，可能会有一定误差。且工具目前仅支持 gcj02 坐标   wgs84
         success(res) {
-          const latitude = res.latitude
-          const longitude = res.longitude
+          that.locLat = res.latitude
+          that.locLng = res.longitude
           const speed = res.speed
           const accuracy = res.accuracy
-          console.log('位置信息'+ res.latitude)
+          console.log('位置信息' + that.locLat + that.locLng)
         }
       })
     },
@@ -33,11 +38,13 @@ Component({
   * 组件的属性列表
   */
   properties: {
-
+  
   },
 
   data: {
-   
+    locLat: '',
+    locLng: '',
+    a: 5
   },
   methods: {
   
